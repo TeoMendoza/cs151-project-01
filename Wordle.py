c9f1ae4fc4a035d047aad2523f6bda26b61c6e46
TeoMendoza <teom4834@gmail.com>
2023-09-27 09:46:15 -0700

    Temp

--- Wordle.py
+++ Wordle.py
@@ -26,7 +26,7 @@ def wordle():
         return optimal_word
     optimal_word = guess_word()
     
-            
+    print(optimal_word)        
     def enter_action():
         """ What should happen when the RETURN or ENTER key is pressed. """
         
@@ -34,33 +34,38 @@ def wordle():
         print(optimal_word)
         print(gw.get_current_row())
         row = 0
-        for col in range(5):
-            x = gw.get_square_letter(row, col)
-            user_word += x
-        user_word = user_word.lower()
-        
-        if user_word in ENGLISH_WORDS:
-            gw.show_message("yay")
-            for i in range(5):
-                if user_word[i] == optimal_word[i]:
-                    gw.set_square_color(row,i, "#66BB66")
-                elif user_word[i] in optimal_word and user_word[i] != optimal_word[i]:
-                    gw.set_square_color(row,i, "#CCBB66")
-                else:
-                    gw.set_square_color(row,i, "#999999")
-        else:
-            for i in range(5):
-                gw.set_square_letter(row, i, " ")
-                gw.set_square_color(row, i, "#FFFFFF")
-                gw.show_message("Not in word list")
-        gw.set_current_row(row)
+        if row  != 6:
+            gw.set_current_row(row)
+            for col in range(5):
+                x = gw.get_square_letter(row, col)
+                user_word += x
+            user_word = user_word.lower()
+            if user_word == optimal_word:
+                gw.show_message("You Win!")
+                gw.set_current_row(5)
+            elif user_word in ENGLISH_WORDS:
+                gw.show_message("yay")
+                for i in range(5):
+                    if user_word[i] == optimal_word[i]:
+                        gw.set_square_color(row,i, "#66BB66")
+                    elif user_word[i] in optimal_word and user_word[i] != optimal_word[i]:
+                        gw.set_square_color(row,i, "#CCBB66")
+                    else:
+                        gw.set_square_color(row,i, "#999999")
+            else:
+                for i in range(5):
+                    gw.set_square_letter(row, i, " ")
+                    gw.set_square_color(row, i, "#FFFFFF")
+                    gw.show_message("Not in word list")  
+            row += 1      
+            
     
     gw = WordleGWindow()
     gw.add_enter_listener(enter_action)
-    # for row in range(5):
-    #     enter_action()
+    
         
 # Startup boilerplate
 if __name__ == "__main__":
+
     wordle()
     
